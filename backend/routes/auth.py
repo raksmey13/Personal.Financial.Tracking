84fc0334f3c67f9ea7b0373f3f8860f43f7dc206
Nim Chanraksmey <nim.chanraksmey@gmail.com>
2026-08-18 18:41:29 +0700

    Change auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -92,7 +92,7 @@ def provision_user_default_categories(session: Session, user_id: int):
 async def send_otp_email(email_to: str, otp_code: str):
     try:
         params: resend.Emails.SendParams = {
-            "from": "PFTrack <onboarding@resend.dev>",
+            "from": "PFTrack <no-reply@pftrack.site>",
             "to": [email_to],
             "subject": "PFTrack Account Verification Code",
             "html": f"""
